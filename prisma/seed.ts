18c7e19c3d8060c5c0b52d1e1e88a1a7a27fd2a2
AdanSerrano <adan.serrano@utp.ac.pa>
2023-10-26 23:34:17 -0500

    build(configure to app): :construction: prisma
    configure prisma file and configuration of tailwind
    
    workin in progress

---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const HASH = 3;
+  const user = [
+    {
+      email: 'adan.serrano@gmail.com',
+      name: 'Adan Serrano',
+      password: '123456',
+    },
+  ];
+
+  for (const item of user) {
+
+    if (item.email && item.name && item.password) {
+      const existingUser = await prisma.user.findUnique({
+        where: { email: item.email },
+      });
+
+      if (existingUser) {
+
+        if (!existingUser.email || !existingUser.name || !existingUser.password) {
+          await prisma.user.delete({
+            where: { email: item.email },
+          });
+        }
+      }
+
+      await prisma.user.upsert({
+        where: { email: item.email },
+        update: {
+          email: item.email,
+          name: item.name,
+          password: await hash(item.password, HASH),
+        },
+        create: {
+          email: item.email,
+          name: item.name,
+          password: await hash(item.password, HASH),
+        },
+      });
+    } else {
+      console.log(`Usuario con campos vacíos: ${JSON.stringify(item)}`);
+    }
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
