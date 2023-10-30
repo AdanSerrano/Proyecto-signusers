a4b68c0152e2e50154c97c6ce7c3287400b8274b
AdanSerrano <adan.serrano@utp.ac.pa>
2023-10-29 22:22:15 -0500

    feat: :truck: configure components file
    workin in progress

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,9 @@
-export default function Loading() {
-    return <p>Loading...</p>;
+import { Spinner } from "@chakra-ui/react";
+
+export default function FullSpinner() {
+    return (
+        <section className="flex justify-center items-center bg-black h-screen">
+            <Spinner speed='3s' size='lg' color="red" width={25} height={25} />
+        </section>
+    );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,13 @@
-import Link from 'next/link';
+import { Flex, Box, Heading, Text, Link } from '@chakra-ui/react';
 
 export default function NotFound() {
     return (
-        <div>
-            <h2>Not Found</h2>
-            <p>Could not find requested resource</p>
-            <Link href="/home">Return Home</Link>
-        </div>
+        <Flex height="100vh" alignItems="center" justifyContent="center">
+            <Box bg="gray.200" p={8} textAlign="center" borderRadius="md">
+                <Heading as="h2" size="md" mb={4}>¡Oops!</Heading>
+                <Text mb={4}>No se pudo encontrar el recurso solicitado.</Text>
+                <Link href="/home" color="blue.500">Volver a Inicio</Link>
+            </Box>
+        </Flex>
     );
 }
