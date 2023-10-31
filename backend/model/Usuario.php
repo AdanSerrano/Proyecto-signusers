43201a26a430bf29eb6c31a5b3527a3d0d97e264
David Maloney <disaacmaloney@gmail.com>
2023-10-30 21:01:54 -0500

    se agregan los sigueintes métodos
       UsuarioExiste: Método para saber si un email ya existe en la base de datos
        UsuarioMostrar: Método para mostrar información de un usuario por correo
        UsuarioModificar: Método para modificar información de un usuario
        UsuarioActualizarImagen: Método para modificar imagen de un usuario
        UsuarioEliminar: Método para eliminar un usuario

--- backend/model/Usuario.php
+++ backend/model/Usuario.php
@@ -11,7 +11,6 @@
         {
             try 
             {
-                // Llamar al procedimiento almacenado
                 $stmt = $this->db->prepare("CALL SP_USUARIO_REGISTRO(:nombre, :email, :password)");
     
                 $stmt->bindParam(':nombre', $nombre, PDO::PARAM_STR);
@@ -39,7 +38,6 @@
         {
             try 
             {
-                // Llamar al procedimiento almacenado
                 $stmt = $this->db->prepare("CALL SP_USUARIO_LISTAR()");
     
                 $stmt->execute();
@@ -62,7 +60,6 @@
         {
             try 
             {
-                // Llamar al procedimiento almacenado
                 $stmt = $this->db->prepare("CALL SP_USUARIO_VERIFICAR_EMAIL(:email)");
     
                 $stmt->bindParam(':email', $email, PDO::PARAM_STR);
@@ -81,6 +78,105 @@
                 return false; 
             }
         } 
+
+        // Método para mostrar información de un usuario por correo
+        public function UsuarioMostrar($email)
+        {
+            try 
+            {
+                $stmt = $this->db->prepare("CALL SP_USUARIO_LISTAR_POR_CORREO(:email)");
+    
+                $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    
+                $stmt->execute();
+
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($result) {
+                    return $result;
+                } else {
+                    return false;
+                }
+                return true; 
+            } catch (PDOException $e) {
+                return false; 
+            }
+        }
+
+        // Método para modificar información de un usuario
+        public function UsuarioModificar($nombre, $email, $password)
+        {
+            try 
+            {
+                $stmt = $this->db->prepare("CALL SP_USUARIO_MODIFICAR(:nombre, :email, :password)");
+    
+                $stmt->bindParam(':nombre', $nombre, PDO::PARAM_STR);
+                $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+                $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+    
+                $stmt->execute();
+
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($result) {
+                    return false;
+                } else {
+                    return true;
+                }
+                return true; 
+            } catch (PDOException $e) {
+                return false; 
+            }
+        }
+
+        // Método para modificar imagen de un usuario
+        public function UsuarioActualizarImagen($email, $imagen)
+        {
+            try 
+            {
+                $stmt = $this->db->prepare("CALL SP_USUARIO_ACTUALIZAR_IMAGEN(:email, :imagen)");
+    
+                $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+                $stmt->bindParam(':imagen', $imagen, PDO::PARAM_STR);
+    
+                $stmt->execute();
+
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($result) {
+                    return false;
+                } else {
+                    return true;
+                }
+                return true; 
+            } catch (PDOException $e) {
+                return false; 
+            }
+        }
+
+        // Método para eliminar un usuario
+        public function UsuarioEliminar($email)
+        {
+            try 
+            {
+                $stmt = $this->db->prepare("CALL SP_USUARIO_ELIMINAR(:email)");
+    
+                $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    
+                $stmt->execute();
+
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($result) {
+                    return false;
+                } else {
+                    return true;
+                }
+                return true; 
+            } catch (PDOException $e) {
+                return false; 
+            }
+        }
 
 
     }
